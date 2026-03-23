d24e88be8ad382a24fa3e22cf379af5d1890b7fe
antoine blanquaert <ablanquaert@pouchain.fr>
2026-03-23 14:33:35 +0100

    feature : dev with dockerize

--- vite.config.js
+++ vite.config.js
@@ -18,13 +18,9 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:5078',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => {
-          //console.log('[PROXY] rewriting:', path);
-          return path.replace(/^\/api/, '');
-        }
       }
     }
   }
